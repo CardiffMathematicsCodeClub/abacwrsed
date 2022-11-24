fd3250cdac99cff3f2a02ceed8c7f16eb61d7953
Michalis <PanayidesM@cardiff.ac.uk>
2022-11-24 14:30:03 +0000

    Make a create_custom_walls function
    Currently the function makes a specific structure of walls.
    At a later branch this function will change to create any structure
    based on some combination of inputs

--- src/main.py
+++ src/main.py
@@ -3,15 +3,7 @@ import math
 
 from camera import CameraGroup
 from player import Player
-from terrain import (
-    Wall_H,
-    Wall_V,
-    Wall_H_JD,
-    Wall_VE_JU,
-    Barrier,
-    SadBarrier,
-    HappyBarrier,
-)
+from utils import create_custom_walls
 
 WIDTH = 1920
 HEIGHT = 1080
@@ -24,32 +16,7 @@ def main():
     clock = pygame.time.Clock()
 
     camera_group = CameraGroup()
-    boxes = pygame.sprite.Group()
-
-    for bx in range(0, 2000, 70):
-        boxes.add(Wall_H(bx, 100, camera_group))  # Wall horizontal top row
-        boxes.add(Wall_H(bx, 1035, camera_group))  # Wall horizontal bottom row
-
-    for bx in range(5):
-        for by in range(170, 660, 70):
-            boxes.add(Wall_V(bx * 490, by, camera_group))  # Wall vertical
-            boxes.add(
-                Wall_VE_JU(bx * 490, 730, camera_group)
-            )  # Wall vertical end blocks
-            boxes.add(
-                Wall_H_JD(bx * 490, 100, camera_group)
-            )  # Wall horizontal top junctions
-        for i in range(3):
-            boxes.add(
-                Wall_H(((i - 1) * 70) + (bx * 490), 660, camera_group)
-            )  # Wall horizontal middle chunks
-        for i in range(3):
-            boxes.add(
-                Barrier((i * 70) + 630, 660, camera_group)
-            )  # Barrier blocking access to one room
-
-        boxes.add(SadBarrier((3 * 70) + 630, 800, camera_group))
-        boxes.add(HappyBarrier((6 * 70) + 630, 800, camera_group))
+    boxes = create_custom_walls(camera_group)
 
     player = Player(
         100, 900, camera_group, boxes
---
+++ src/utils.py
@@ -0,0 +1,40 @@
+import pygame
+
+from terrain import (
+    Wall_H,
+    Wall_V,
+    Wall_H_JD,
+    Wall_VE_JU,
+    Barrier,
+    SadBarrier,
+    HappyBarrier,
+)
+
+def create_custom_walls(camera_group):
+    boxes = pygame.sprite.Group()
+    for bx in range(0, 2000, 70):
+        boxes.add(Wall_H(bx, 100, camera_group))
+        boxes.add(Wall_H(bx, 1035, camera_group))
+
+    for bx in range(5):
+        for by in range(170, 660, 70):
+            boxes.add(Wall_V(bx * 490, by, camera_group))
+            boxes.add(
+                Wall_VE_JU(bx * 490, 730, camera_group)
+            )
+            boxes.add(
+                Wall_H_JD(bx * 490, 100, camera_group)
+            )
+        for i in range(3):
+            boxes.add(
+                Wall_H(((i - 1) * 70) + (bx * 490), 660, camera_group)
+            )
+        for i in range(3):
+            boxes.add(
+                Barrier((i * 70) + 630, 660, camera_group)
+            )
+
+        boxes.add(SadBarrier((3 * 70) + 630, 800, camera_group))
+        boxes.add(HappyBarrier((6 * 70) + 630, 800, camera_group))
+
+    return boxes
