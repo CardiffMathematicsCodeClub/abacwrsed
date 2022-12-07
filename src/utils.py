9a9ed316127320ed27eeb790702910caefd24549
Vince Knight <vincent.knight@gmail.com>
2022-12-07 12:29:06 +0000

    Add more rooms (#18)
    * Further modify utils.
    
    * Add more rooms.
    
    All rooms can now just be added
    by filling in the config file.

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,7 @@ from terrain import (
     HappyBarrier,
 )
 
-def get_building_argumets(room):
+def get_building_parameters(config_path):
     """
     Function takes .yml file for specific room with:
     - position : (x, y) - left upper corner
@@ -21,21 +21,24 @@ def get_building_argumets(room):
     Variable room contains path to .yml file with that room configuration.
     """
 
-    with open(room, 'r') as f:
+    with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
-    print(config)
 
-    position = (int(config['position_x']), int(config['position_y']))
-    length = int(config['length'])
-    height = int(config['height'])
+    parameters = {}
+    for room in config:
+        for room_id, value in room.items():
+            room_parameters = value
+            position = (int(room_parameters['position_x']), int(room_parameters['position_y']))
+            length = int(room_parameters['length'])
+            height = int(room_parameters['height'])
+            parameters[room_id] = (position, length, height)
 
-    return position, length, height
+    return parameters
 
-def building_configuration(boxes, camera_group, room):
+def building_configuration(boxes, camera_group, position, length, height):
     """
     Builds a room from position, length and height
     """
-    position, length, height = get_building_argumets(room)
     x, y = position
 
     for dx in range(0, length, 70):
@@ -53,7 +56,10 @@ def create_custom_walls(camera_group):
     Creates a custom set of walls for the game
     """
     boxes = pygame.sprite.Group()
-    boxes = building_configuration(boxes, camera_group, 'Rooms\hub.yml')
+    config_path = "./rooms/hub.yml"
+    parameters = get_building_parameters(config_path=config_path)
+    for _, (position, length, height) in parameters.items():
+        boxes.add(building_configuration(boxes=boxes, camera_group=camera_group, position=position, length=length, height=height))
 
 
     return boxes
